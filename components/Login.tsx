6d76caa80fb4a478d1cc071214d022ca686b1728
Seth Zea <sz16900@my.bristol.ac.uk>
2023-01-04 17:11:53 -1000

    stuck with setting up session

---
+++ components/Login.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import React from 'react';
+import { signIn } from 'next-auth/react';
+
+function Login() {
+  return (
+    <div className="grid place-items-center">
+      <Image
+        src="https://links.papareact.com/t4i"
+        width={400}
+        height={400}
+        alt=""
+      />
+      <h1
+        onClick={() => signIn()}
+        className="p-5 bg-blue-500 rounded-full text-white cursor-pointer text-center"
+      >
+        Login with Facebok
+      </h1>
+    </div>
+  );
+}
+
+export default Login;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '../components/Header'
+import type { NextPage } from 'next';
+import { useSession } from 'next-auth/react';
+import Head from 'next/head';
+import Header from '../components/Header';
+import Login from '../components/Login';
 
-const Home: NextPage = () => {
+// this works but whats up with this error?
+const Home: NextPage = (session) => {
+  if (!session) return <Login />;
+
   return (
     <div className="">
       <Head>
@@ -18,9 +23,19 @@ const Home: NextPage = () => {
         {/* Feed */}
         {/* Widget */}
       </main>
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+export async function getServerSideProps(context: any) {
+  // Get the user
+  const { data: session } = useSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
