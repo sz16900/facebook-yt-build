c00f95f4132fe97a621135e191cef3cb1a41baa9
Seth Zea <sz16900@my.bristol.ac.uk>
2023-01-23 11:39:54 -1000

    Post done!

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
+import {
+  ChatBubbleOvalLeftIcon,
+  ShareIcon,
+  HandThumbUpIcon,
+} from '@heroicons/react/24/outline';
 
 function Post({ name, message, email, postImage, image, timestamp }: any) {
   return (
@@ -24,10 +29,25 @@ function Post({ name, message, email, postImage, image, timestamp }: any) {
 
       {/* ONLY OF THERE IS AN IMAGE */}
       {postImage && (
-        <div className="relative h-56 md:h-96 bg-white">
+        <div className="relative h-56 md:h-96 bg-white overflow-hidden">
           <Image src={postImage} width={500} height={500} alt="" />
         </div>
       )}
+      {/* Footer of the Posst */}
+      <div className="flex justify-between items-center rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <HandThumbUpIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-bl-2xl">
+          <ChatBubbleOvalLeftIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+        <div className="inputIcon rounded-none rounded-br-2xl">
+          <ShareIcon className="h-4" />
+          <p className="text-xs sm:text-base">Like</p>
+        </div>
+      </div>
     </div>
   );
 }
